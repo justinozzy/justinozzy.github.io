612a631f350521df153a5a6989714b72e46fcdb2
justinozzy <jetoswald@gmail.com>
2024-07-20 06:54:00 -0700

    Added daisyUI and theme changer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,15 @@
 import Image from "next/image";
+import ThemeComponent from "@/components/ThemeComponent";
 
 export default function Home() {
   return (
-    <div>hello wrrrd!</div>
+    <div className="navbar bg-base-100">
+      <div className="flex-1">
+        <a className="btn btn-ghost text-xl">daisyUI</a>
+      </div>
+      <div className="flex-none">
+        <ThemeComponent />
+      </div>
+    </div>
   );
 }
